22283aa28266338fbd34ca2e9a28efa853b75472
radekaadek <radekaadek@gmail.com>
2024-01-06 00:21:44 +0100

    experimental data

--- es.py
+++
@@ -1,7 +0,0 @@
-# print the first 1000 characters of warsaw_buildings.json
-# dont load the whole file into memory
-
-# read as a text file
-with open('virginia_buildings.json', 'r') as f:
-    data = f.read()
-    print(data[:1000])
