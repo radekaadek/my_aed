e16ba58187128fc82c2590167bb3aa95a5944967
radekaadek <radekaadek@gmail.com>
2024-01-08 10:43:22 +0100

    modeltrainer

--- train_model.py
+++ train_model.py
@@ -18,31 +18,29 @@ h2o_df = h2o.H2OFrame(main_df)
 x = list(main_df.columns)
 y = 'OHCA'
 
-aml = H2OAutoML(max_models=20, seed=1, max_runtime_secs=3600)
+aml = H2OAutoML(max_models=20, seed=1, max_runtime_secs=10)
 aml.train(x=x, y=y, training_frame=h2o_df)
 
 lb = aml.leaderboard
 # save leaderboard to csv
 # check if a leader was found
 if lb is not None:
-    # save leaderboard
+    # save leaderboard to csv
     lb_df = lb.as_data_frame()
     lb_df.to_csv('leaderboard.csv')
-    # save leader model
-    leader_model = aml.leader
-    leader_model.save_mojo('leader_model')
-    # save leader model params
-    leader_model_params = leader_model.params
-    leader_model_params.to_csv('leader_model_params.csv')
-else:
-    print('No leader model found')
-    raise Exception('No leader model found')
 
-# Predict
-h2o_target_df = h2o.H2OFrame(target_df)
-pred = leader_model.predict(h2o_target_df)
-pred_df = pred.as_data_frame()
-pred_df.to_csv('pred.csv')
+# Get the best model
+leader_model = aml.leader
+leader_model.download_mojo(path='./', get_genmodel_jar=True)
+
+# Predict and add to target csv
+target_df = h2o.H2OFrame(target_df)
+predictions = leader_model.predict(target_df)
+predictions_df = predictions.as_data_frame()
+target_df = target_df.as_data_frame()
+target_df['predictions'] = predictions_df
+target_df.to_csv('predictions.csv')
+
 
 # Shutdown h2o
 h2o.cluster().shutdown()
