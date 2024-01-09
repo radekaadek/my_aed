a4138ad2556d47c58ba91dc6c023f2d8fc309f70
radekaadek <radekaadek@gmail.com>
2024-01-09 17:05:49 +0100

    jazda

--- aquire_data.py
+++ aquire_data.py
@@ -36,10 +36,84 @@ def get_point_data(node_name: str, area_name: str, date: str = None) -> list[dic
     resp = r.json()
     nodes = []
     for node in resp['elements']:
+        # print(node)
         node_geom = (node["lat"], node["lon"])
         name = node["tags"][node_name]
         nodes.append({"geometry": node_geom, "type": name})
     return nodes
+
+def get_line_data(node_name: str, area_name: str, date: str = None) -> list[dict]:
+    """Get line data from Overpass API in the form of a list of dictionaries:
+
+    geometry: [(lat, lon), (lat, lon), ...], type: node_name
+
+    Keyword arguments:
+
+    node_name -- name of the node to get data from
+    area_name -- name of the area to get data from
+    date -- date of the data (default None)
+    """
+    if date is None:
+        # set to current date
+        date = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
+    # ovapi = Overpass()
+    nomatim = Nominatim()
+    areaid = nomatim.query(area_name).areaId()
+    query = overpassQueryBuilder(area=areaid, elementType='way', selector=node_name, out='body geom')
+    query = '[out:json][timeout:900];' + query
+    print(f"Query: {query}")
+    # r = ovapi.query(query, date=date, timeout=900)
+    base_url = "https://overpass-api.de/api/interpreter"
+    # body is query
+    r = requests.post(base_url, data=query, timeout=900)
+    # save to file for debugging
+    resp = r.json()
+    # save to file for debugging
+    ways = []
+    for way in resp['elements']:
+        el = {"geometry": way["geometry"], "type": way["tags"][node_name]}
+        ways.append(el)
+    return ways
+
+def get_line_df(line_data: list[dict]) -> pd.DataFrame:
+    """Convert line data to a DataFrame with columns:
+
+    name, sum of line lengths
+
+    Indexed by hexagon id.
+
+    Keyword arguments:
+
+    line_data -- list of dictionaries with line data
+    """
+    # input data:
+    # [{'geometry': [{'lat': 50.2417639, 'lon': 20.723122}, {'lat': 50.2417077, 'lon': 20.7231886}, {'lat': 50.2411756, 'lon': 20.7238183}], 'type': 'highway'}, {'geometry': [{'lat': 50.24042, 'lon': 20.7183544}, {'lat': 50.2404546, 'lon': 20.718521}, {'lat': 50.2404714, 'lon': 20.718619}, {'lat': 50.2405076, 'lon': 20.7188718}, {'lat': 50.2406439, 'lon': 20.7200494}, {'lat': 50.2406854, 'lon': 20.720379}, {'lat': 50.2407154, 'lon': 20.7205757}, {'lat': 50.2407444, 'lon': 20.720717}, {'lat': 50.240808, 'lon': 20.7209736}, {'lat': 50.2408799, 'lon': 20.7211982}, {'lat': 50.2410184, 'lon': 20.7215398}, {'lat': 50.2413235, 'lon': 20.722232}, {'lat': 50.2415226, 'lon': 20.7226517}, {'lat': 50.2416862, 'lon': 20.7229742}, {'lat': 50.2417639, 'lon': 20.723122}], 'type': 'highway'}, {'geometry': [{'lat': 50.2425086, 'lon': 20.7258192}, {'lat': 50.2430309, 'lon': 20.7258879}, {'lat': 50.2430919, 'lon': 20.7258524}],
+    # create a dictionary
+    retv = {}
+    for line in line_data:
+        hexes = set()
+        for point in line['geometry']:
+            hexes.add(h3.latlng_to_cell(point['lat'], point['lon'], 9))
+        for hexagon in hexes:
+            h3_input = [(point['lat'], point['lon']) for point in line['geometry']]
+            len = line_hexagon_length(h3_input, hexagon)
+            name = line['type']
+            if hexagon in retv.keys():
+                if name in retv[hexagon].keys():
+                    retv[hexagon][name] += len
+                else:
+                    retv[hexagon][name] = len
+            else:
+                retv[hexagon] = {name: len}
+
+    # convert the dictionary to a dataframe
+    retv = pd.DataFrame(retv).T
+    # set index Name
+    retv.index.name = 'hex_id'
+    # fill NaNs with 0s
+    retv = retv.fillna(0)
+    return retv
+    
 
 def get_area_data(area_name: str, selector: str = 'building', tags_to_look_for: list[str] = ['amenity', 'building'], date: str = None) -> pd.DataFrame:
     """Get area data from Overpass API in the form of a pandas DataFrame:
@@ -137,6 +211,28 @@ def cells_to_polygon(cells: set[str]) -> shapely.geometry.Polygon:
     retv = shapely.geometry.Polygon(h3_input)
     return retv
 
+def line_hexagon_length(line: list[tuple[float, float]], hexagon: str) -> float:
+    """Calculate the length of a line that is inside a hexagon.
+
+    Keyword arguments:
+
+    line -- list of tuples (lat, lon)
+    hexagon -- hexagon id
+    """
+    # get the intersection of the line and the hexagon
+    # convert to a list of tuples
+    hex_bpoly_4326 = cells_to_polygon({hexagon})
+    # get polygon coords
+    x, y = hex_bpoly_4326.exterior.coords.xy
+    # create a polygon
+    hex_bpoly = shapely.geometry.Polygon(zip(x, y))
+    # get area of the intersection
+    intersection = hex_bpoly.intersection(shapely.geometry.LineString(line))
+    return intersection.length
+
+# def get_line_df(line_df: pd.DataFrame, hexagon_res: int = 9) -> pd.DataFrame:
+
+
 def get_area_df(building_df: pd.DataFrame, hexagon_res: int = 9) -> pd.DataFrame:
     """Calculate area of buildings for hexagon grid, return a GeoDataFrame:
 
@@ -147,7 +243,7 @@ def get_area_df(building_df: pd.DataFrame, hexagon_res: int = 9) -> pd.DataFrame
 
     Keyword arguments:
 
-    building_gdf -- GeoDataFrame with building data
+    building_df -- DataFrame with building data
     hexagon_res -- resolution of the hexagon grid (default 9)
     """
     # input df:
@@ -245,16 +341,21 @@ def get_all_data(area_name: str, hexagon_res: int = 9, get_neighbours: bool = Tr
     building_df = get_area_data(area_name, selector='building', tags_to_look_for=['amenity', 'building'], date=date)
     landuse_df = get_area_data(area_name, selector='landuse', tags_to_look_for=['landuse'], date=date)
     leisure_df = get_area_data(area_name, selector='leisure', tags_to_look_for=['leisure'], date=date)
+
+    line_data = get_line_data('highway', area_name, date=date)
+    line_df = get_line_df(line_data)
+
     s = time.time()
     building_area_df = get_area_df(building_df, hexagon_res)
     landuse_area_df = get_area_df(landuse_df, hexagon_res)
     leisure_area_df = get_area_df(leisure_df, hexagon_res)
     e = time.time()
     print(f"Time to get area data: {e-s}")
-    # merge the three dataframes
+    # merge the dfs
     retv = pd.merge(feature_df, building_area_df, on='hex_id', how='outer')
     retv = pd.merge(retv, landuse_area_df, on='hex_id', how='outer')
     retv = pd.merge(retv, leisure_area_df, on='hex_id', how='outer')
+    retv = pd.merge(retv, line_df, on='hex_id', how='outer')
     # set index to hex_id
     retv = retv.set_index('hex_id')
     # fill NaNs with 0
@@ -280,6 +381,4 @@ if __name__ == "__main__":
     target = get_all_data("Warszawa")
     target.to_csv('warszawa_osm.csv')
     # test get all data
-    # sochocin = get_all_data("Sochocin", hexagon_res=9, get_neighbours=True)
-    # print(sochocin)
     pass
