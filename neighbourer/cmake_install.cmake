399828d7026093c945f49cd780106629097660e9
radekaadek <radekaadek@gmail.com>
2024-01-14 17:30:02 +0100

    commitment2

--- neighbourer/cmake_install.cmake
+++ neighbourer/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/radekaadek/my_aed/neighbourer
+# Install script for directory: /home/CENAGIS/rdabkowski/my_aed/neighbourer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -39,7 +39,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/radekaadek/my_aed/neighbourer/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/CENAGIS/rdabkowski/my_aed/neighbourer/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
